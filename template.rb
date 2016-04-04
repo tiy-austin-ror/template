e2727ed28f14873a09a381899fd38163036f0b07
Justin Herrick <justin@justinherrick.com>
2016-04-04 11:57:32 -0500

    Hard coding the ruby version and adding a version constant.
    Also moving around gems.

--- template.rb
+++ template.rb
@@ -1,17 +1,10 @@
 # Setup steps for a rails project
-#
-ruby_version = ask("What is your ruby version? ")
+
+VERSION = '0.1.1'
+
+ruby_version = '2.3.0'
 insert_into_file "Gemfile", "ruby '#{ruby_version}'", after: "source 'https://rubygems.org'\n"
 
-rake("db:create")
-gem 'faker'
-gem 'kaminari'
-gem 'bcrypt'
-
-gem_group :production do
-  gem 'puma'
-  gem 'rails_12factor'
-end
 
 if yes?("Using Bootstrap?")
   gem 'bootstrap-sass', '~> 3.3.5'
@@ -21,16 +14,29 @@ else
   end
 end
 
+gem 'faker'
+gem 'kaminari'
+gem 'bcrypt'
+
+gem_group :production do
+  gem 'puma'
+  gem 'rails_12factor'
+end
+
+
 file 'Procfile', "web: bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RACK_ENV:-development}"
 
 run 'mv README.rdoc README.md'
+
 inside('app/assets/stylesheets') do
   run "mv application.css application.scss"
 end
 
+rake("db:create")
+
 after_bundle do
   git :init
   git add: "."
-  git commit: %Q{ -m 'Initial commit of TIY Rails Generator v0.1.0' }
+  git commit: %Q{ -m 'Initial commit of TIY Rails Generator v#{VERSION}' }
 end
 
